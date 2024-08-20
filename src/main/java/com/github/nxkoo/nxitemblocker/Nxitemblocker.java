b372535b9ca82e7363b75d325229019e4721ebdf
NykooX <nykoofy@proton.me>
2024-08-20 19:41:57 -0300

    Finish commit

--- src/main/java/com/github/nxkoo/nxitemblocker/Nxitemblocker.java
+++ src/main/java/com/github/nxkoo/nxitemblocker/Nxitemblocker.java
@@ -1,15 +1,29 @@
 package com.github.nxkoo.nxitemblocker;
 
+import com.github.nxkoo.nxitemblocker.lib.SimpleConfig;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+
+import java.util.List;
 
 public class Nxitemblocker implements ModInitializer {
-    private final List<String> blockedItems = Arrays.asList(
-            "minecraft:iron_sword",
-            "minecraft:diamond_sword"
-    );
-
+    private final SimpleConfig CONFIG = SimpleConfig.of("nxitemblocker").provider(this::provider).request();
+    private String provider(String pvd) {
+        return "nxitemblocker";
+    }
+    private final List<String> blockedItems = CONFIG.getStringList("blocked_items");
     @Override
     public void onInitialize() {
-
+        UseEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+            if (entity instanceof ItemFrameEntity) {
+                ItemStack itemStack = player.getStackInHand(hand);
+                if (itemStack.isEmpty()) return ActionResult.PASS;
+                if (blockedItems.contains(itemStack.getItem().toString())) return ActionResult.FAIL;
+            }
+            return ActionResult.PASS;
+        });
     }
 }
