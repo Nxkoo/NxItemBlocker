65fec471df877fbd205691deaa0d51768766a9cc
NykooX <nykoofy@proton.me>
2024-08-29 20:27:36 -0300

    🚀 | finalized - version 1.18.2

---
+++ NxItemBlocker-1.18.2/src/main/java/com/github/nxkoo/nxitemblocker/NxItemBlocker.java
@@ -0,0 +1,10 @@
+package com.github.nxkoo.nxitemblocker;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NxItemBlocker implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
